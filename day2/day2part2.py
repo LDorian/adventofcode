9221feceb5ef78afd865d7353e98662740181052
Dorian Lamour <lamourdorian@outlook.fr>
2021-12-05 23:47:31 +0100

    format changes

--- day2/day2part2.py
+++ day2/day2part2.py
@@ -1,8 +1,10 @@
 import os
 
+
 def openfile():
-    file = open(os.path.join(os.path.dirname(__file__), 'day2input.txt'), 'r')
+    file = open(os.path.join(os.path.dirname(__file__), "day2input.txt"), "r")
     return file
+
 
 def submarine_move(file):
     x = 0
@@ -10,18 +12,20 @@ def submarine_move(file):
     aim = 0
     for line in file:
         line = line.strip()
-        if line[0] == 'f':
+        if line[0] == "f":
             x += int(line[-1])
             y += aim * int(line[-1])
-        elif line[0] == 'u':
+        elif line[0] == "u":
             aim -= int(line[-1])
-        elif line[0] == 'd':
+        elif line[0] == "d":
             aim += int(line[-1])
     return x * y
+
 
 def main():
     file = openfile()
     print(submarine_move(file))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
