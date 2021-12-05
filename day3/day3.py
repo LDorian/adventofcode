8710898d094545acd78baa9d7ff2b7c708c4db0a
Dorian Lamour <lamourdorian@outlook.fr>
2021-12-05 23:47:41 +0100

    changes

--- day3/day3.py
+++ day3/day3.py
@@ -1,26 +1,30 @@
 def openfile():
-    file = open('day3/day3input.txt', 'r')
+    file = open("day3/day3input.txt", "r")
     return file
+
 
 def count_bits(file):
     bits_array = []
     for binary in [line.strip() for line in file.readlines()]:
         for i in range(len(binary)):
-            if(len(bits_array) < i+1):
+            if len(bits_array) < i + 1:
                 bits_array.append(0)
-            if(binary[i] == '1'):
+            if binary[i] == "1":
                 bits_array[i] += 1
             else:
                 bits_array[i] -= 1
     return bits_array
 
+
 def gamma(bits_array):
-    gamma_array = ['1' if x > 0 else '0' for x in bits_array]
-    return int(''.join(gamma_array), 2)
+    gamma_array = ["1" if x > 0 else "0" for x in bits_array]
+    return int("".join(gamma_array), 2)
+
 
 def epsilon(bits_array):
-    epsilon_array = ['1' if x <= 0 else '0' for x in bits_array]
-    return int(''.join(epsilon_array), 2)
+    epsilon_array = ["1" if x <= 0 else "0" for x in bits_array]
+    return int("".join(epsilon_array), 2)
+
 
 def main():
     file = openfile()
@@ -28,5 +32,6 @@ def main():
     result = gamma(bits_array) * epsilon(bits_array)
     print(str(result))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
