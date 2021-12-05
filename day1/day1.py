198f78b9597816c9a919032d25f7a61c7c46adaa
Dorian Lamour <lamourdorian@outlook.fr>
2021-12-05 23:47:14 +0100

    few changes

--- day1/day1.py
+++ day1/day1.py
@@ -1,21 +1,25 @@
 import os
 
+
 def openfile():
-    file = open(os.path.join(os.path.dirname(__file__), 'day1input.txt'), 'r')
+    file = open(os.path.join(os.path.dirname(__file__), "day1input.txt"), "r")
     return file
+
 
 def count_increase(file):
     count = 0
-    cpt = 9999 # to avoid first line to be counted
+    cpt = 9999  # to avoid first line to be counted
     for line in file:
         if cpt < int(line):
             count += 1
         cpt = int(line)
     return count
 
+
 def __main__():
     file = openfile()
     print(count_increase(file))
+
 
 if __name__ == "__main__":
     __main__()
--- day1/day1part2.py
+++ day1/day1part2.py
@@ -1,34 +1,43 @@
 import os
 
+
 def openfile():
-    file = open(os.path.join(os.path.dirname(__file__), 'day1input.txt'), 'r')
+    file = open(os.path.join(os.path.dirname(__file__), "day1input.txt"), "r")
     return file
+
 
 def closefile(file):
     file.close()
 
+
 def sliding_window(file):
     count = 0
     line_cpt = 0
-    cpt = 9999 # to avoid first line to be counted
+    cpt = 9999  # to avoid first line to be counted
 
     lines = file.readlines()
 
     for line in lines:
         line_cpt += 1
-        if(line_cpt < len(lines) - 1):
-            sum1 = int(line.strip()) + int(lines[line_cpt].strip()) + int(lines[line_cpt+1].strip())
-            #sum2 = int(line[line_cpt].strip()) + int(lines[line_cpt+1].strip()) + int(lines[line_cpt+2].strip())
+        if line_cpt < len(lines) - 1:
+            sum1 = (
+                int(line.strip())
+                + int(lines[line_cpt].strip())
+                + int(lines[line_cpt + 1].strip())
+            )
+            # sum2 = int(line[line_cpt].strip()) + int(lines[line_cpt+1].strip()) + int(lines[line_cpt+2].strip())
             print(cpt, sum1)
-            if(sum1 > cpt):
+            if sum1 > cpt:
                 count += 1
             cpt = sum1
     return count
+
 
 def __main__():
     file = openfile()
     print(sliding_window(file))
     closefile(file)
+
 
 if __name__ == "__main__":
     __main__()
